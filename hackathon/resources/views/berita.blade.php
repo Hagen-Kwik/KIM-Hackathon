fe9712aa6f7a12e70625b3cd65d4a157d5092d4c
Hagen-Kwik <hagen.kwik@gmail.com>
2023-09-22 21:10:34 +0700

    Update Routing & Nav

---
+++ hackathon/resources/views/berita.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.mainheader')
+@section('css')
+    <link rel="stylesheet" href="{{ asset('styles/.css') }}">
+@endsection
+@section('title', 'Berita')
+
+@section('content')
+    <div class="container mt-5">
+        <h1 style="color: black;">TEST</h1>
+        <a href="">testingnggngngng</a>
+    </div>
+@endsection
