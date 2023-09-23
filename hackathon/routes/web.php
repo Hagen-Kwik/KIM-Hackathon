ccd69e132c9000e1413737aa711b8da0f6145b88
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-09-23 15:56:26 +0700

    import beranda controller web

--- hackathon/routes/web.php
+++ hackathon/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\podcastController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NewsController;
