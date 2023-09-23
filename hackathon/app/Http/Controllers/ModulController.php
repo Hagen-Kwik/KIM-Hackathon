2f2c1766e3f2968d30411a20e057eb4755858753
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-09-24 02:17:31 +0700

    bug fix

--- hackathon/app/Http/Controllers/ModulController.php
+++ hackathon/app/Http/Controllers/ModulController.php
@@ -9,6 +9,7 @@ use App\Models\Quiz;
 use App\Models\Teacher;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ModulController extends Controller
 {
